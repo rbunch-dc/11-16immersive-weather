ded26f5eb4370ee4b771aeabeb159e77142cf87e
Robert Bunch <rob@digitalcrafts.com>
2016-12-20 14:41:51 -0500

    Add weather api

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
 
-	var weatherUrl = 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid='+apiKey;
-	console.log(weatherUrl);
-	$.getJSON(weatherUrl, function(weatherData){
-		console.log(weatherData);
-	});
+	$('#weather-form').submit(function(){
+		// stop the form from submitting!
+		event.preventDefault();
+		// Inout field has id of lcoation. go get it.
+		var location = $('#location').val();
+		var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&zip='+location+',us&appid='+apiKey;
+		console.log(weatherUrl);
+		$.getJSON(weatherUrl, function(weatherData){
+			// console.log(this);
+			console.log(weatherData);
+			var currTemp = weatherData.main.temp;
+			var name = weatherData.name;
+			$('#currTemp').html("The temp in " + name + " is currently " +currTemp);
+			// console.log(currTemp);
+		});
+
+	})
+
 
 });
 // console.log(apiKey);
