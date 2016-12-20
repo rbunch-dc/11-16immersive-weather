b50997aed61a968bef65c396bfb235d0c3dd79f3
Robert Bunch <rob@digitalcrafts.com>
2016-12-20 14:14:02 -0500

    add weather stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log(apiKey);
+$(document).ready(function(){
+
+	var weatherUrl = 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid='+apiKey;
+	console.log(weatherUrl);
+	$.getJSON(weatherUrl, function(weatherData){
+		console.log(weatherData);
+	});
+
+});
+// console.log(apiKey);
